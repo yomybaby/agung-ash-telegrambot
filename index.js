e3a1701267dbd015ba29e9bdd1848d47363ed6b4
Jong Eun Lee <yomybaby@yomybaby.com>
2017-12-03 02:01:59 +0800

    `alert new forecast` feature

--- index.js
+++ index.js
@@ -18,15 +18,13 @@ moment.tz.setDefault('Asia/Brunei'); //+08:00 Bali
 
 // redis.hgetall('chatList').then(function (result) {
 //   console.log(result);
-//   _.each(result, (value, chatId)=>{
-//     bot.telegram.sendMessage(chatId, value);
-//   });
 // });
 
 // update ash data every 5 minutes
 setInterval(function () {
   console.log('updated at ' + moment().format('LLL'));
   updateAshData();
+  rp(process.env.SERVER_URL);
 },5*60*1000);
 
 updateAshData().then(function (){
@@ -35,56 +33,83 @@ updateAshData().then(function (){
     ctx.replyWithMarkdown(
 `Welcome!👐
 
-I am Agung Ash Bot for your safety.
-I will let you know wether you are in ash area or not based on ash prediction data from [bom.gov.au](http://www.bom.gov.au/products/IDD65300.shtml). *I'm not sure the result from me is correct. Please check again* [original source](http://www.bom.gov.au/products/IDD65300.shtml).
+You can check whether your current location is in Mt. Agung's ash cloud coverage or not.
+this info is based on a data from [bom.gov.au](http://www.bom.gov.au/products/IDD65300.shtml). 
 
-Take care! 🙏`);
-    return ctx.replyWithMarkdown('Avaliable commands:\n/check\nCheck current location ash state.\n\n/developer\nAre you a developer? please make this bot together', Telegraf.Extra.markup((markup) => {
+Be safe and enjoy beautiful Bali! 🙏`);
+    return ctx.replyWithMarkdown('Avaliable commands:\n/check\nCheck current location.\n\n/developer\nAre you a developer? please make this bot together', Telegraf.Extra.markup((markup) => {
       return markup.resize()
       .keyboard([
-        markup.locationRequestButton('📍 Check my location 😷 or 😃')
+        markup.locationRequestButton('📍 Check my location')
       ])
     }));
   })
-  bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
   bot.command('developer', (ctx) => ctx.replyWithMarkdown(`I'm made by JongEun Lee. My source code is available on [github](https://github.com/yomybaby/agung-ash-telegramBot).\n\nIf you want buy 🍺 for Jong, go Outpost and find him. 🙏`));
-  bot.command('check', (ctx) => ctx.replyWithMarkdown('Please, press a button on the bottom.', Telegraf.Extra.markup((markup) => {
+  bot.command('check', (ctx) => ctx.replyWithMarkdown('PPlease, press "Check my location" button on the bottom to start.', Telegraf.Extra.markup((markup) => {
     return markup.resize()
     .keyboard([
-      markup.locationRequestButton('📍 Check my location 😷 or 😃')
+      markup.locationRequestButton('📍 Check my location')
     ])
     // .oneTime()
   }))
   );
-  bot.on('location',  (ctx) => {
+  bot.on('location',  async (ctx) => {
     const {location} = ctx.message;
 
     const currentLocation = [location.latitude, location.longitude];
 
-    let message = '*Your Location Ash Prediction*\n\n';
-    const ashState = [];
-    latestAshData.forEach(function (data) {
-      // message+=`${data.hours?('+'+data.hours+'h'):'current'} : ${
-      let isInside = inside( currentLocation, data.pointers );
-      ashState.push(isInside);
-      message+=`${
-        isInside?'😷':'😃'
-      } ${updatedMoment.clone().add(data.hours,'h').format('HH:mma DD/MM')}\n`
-    })
-    message +=`\nsource from [bom.gov.au](http://www.bom.gov.au/products/IDD65300.shtml) at ${updatedMoment.format('LLL')}`
+    let message = `*Check this forecast for Mt. Agung's ash coverage.*\n\n`;
+    
+    const generatedAshInfo = generateAshInfo(currentLocation);
+    message+=generatedAshInfo.message;
+    message +=`\nYou may check it on [the ash cloud map of bom.gov.au](http://www.bom.gov.au/products/IDD65300.shtml) at ${updatedMoment.format('LT DD/MM')}\n\nMay I alert you when a new forecast comes out(every 3 hours)?`
     
     const currentChatInfo = {};
+    
+    const previousInfo = JSON.parse( await redis.hget('chatList', ctx.message.chat.id) || {});
     currentChatInfo[ctx.message.chat.id] = JSON.stringify({
-      ashState,
-      location : currentLocation
+      ashState : generatedAshInfo.ashState,
+      location : currentLocation,
+      alertOnNew : previousInfo.alertOnNew
     });
     redis.hmset('chatList', currentChatInfo);
-    
-    ctx.message.chat.id
-    return ctx.replyWithMarkdown(message);
+    ctx.state.savedChatInfo = currentChatInfo[ctx.message.chat.id];
+    return ctx.replyWithMarkdown(message, Telegraf.Markup
+      .keyboard([
+        ['Yes, alert me'], // Row1 with 2 buttons
+        ['No, stop it' ]
+      ])
+      .oneTime()
+      .resize()
+      .extra()
+    );
     // http://www.bom.gov.au/products/IDD65300.shtml
 
-  })
+  });
+  
+  bot.hears('No, stop it', async (ctx)=>{
+    await redis.hget('chatList', ctx.message.chat.id).then(function (result) {
+      const obj = JSON.parse(result);
+      obj.alertOnNew = false;
+      const currentChatInfo = {};
+      currentChatInfo[ctx.message.chat.id] = JSON.stringify(obj);
+      redis.hmset('chatList', currentChatInfo);
+    });
+    
+    return ctx.reply('OK Thanks! If you want again. Please run /check again.')
+  });
+  
+  bot.hears('Yes, alert me', async (ctx)=>{
+    await redis.hget('chatList', ctx.message.chat.id).then(function (result) {
+      const obj = JSON.parse(result);
+      obj.alertOnNew = true;
+      const currentChatInfo = {};
+      currentChatInfo[ctx.message.chat.id] = JSON.stringify(obj);
+      redis.hmset('chatList', currentChatInfo);
+    })
+    
+    return ctx.reply('Great! I\'ll let you know.')
+  });
 
   if(process.env.NODE_ENV === 'production') {
 
@@ -107,6 +132,7 @@ async function updateAshData() {
 
     updatedMoment = moment(/DTG: (.*)/.exec(preText)[1], 'YYYYMMDD/HHmmZ');
     
+    
     redis.get('dataUpdatedAt').then(function (result) {
       if(result !== updatedMoment.toISOString()){ // if new ash data is comming
         console.log('NEW DATA IS COMMING');
@@ -115,18 +141,16 @@ async function updateAshData() {
             let chatInfo = JSON.parse(value);
             
             if(chatInfo.location){
-              const currentState = [];
-              let message = '*Prediction has beend changed.\n\n';
-              latestAshData.forEach(function (data) {
-                let isInside = inside( chatInfo.location, data.pointers );
-                currentState.push(isInside);
-                message+=`${
-                  isInside?'😷':'😃'
-                } ${updatedMoment.clone().add(data.hours,'h').format('HH:mma DD/MM')}\n`
-              });
+              const generatedAshInfo = generateAshInfo(chatInfo.location);
               
-              if(!_.isEqual(currentState, chatInfo.ashState)){
-                bot.telegram.sendMessage(chatId, message);
+              // if(chatInfo.alertOnNew || (chatInfo.alertOnChange && !_.isEqual(generatedAshInfo.ashState, chatInfo.ashState))){
+              if(chatInfo.alertOnNew){
+                let message = `*New prediction published.*\n${generatedAshInfo.emoMessage}\n\n`
+                + generatedAshInfo.message;
+                bot.telegram.sendMessage(chatId, message, {
+                  parse_mode: 'Markdown'
+                });
+                message +=`\nYou may check it on [the ash cloud map of bom.gov.au](http://www.bom.gov.au/products/IDD65300.shtml) at ${updatedMoment.format('LT DD/MM')}`
               }
             }
           })
@@ -182,4 +206,23 @@ function convert2Pointer(dataStr) {
       dms2Decimal(arr[1]),
     ]
   });
+}
+
+function generateAshInfo(currentLocation){
+  let message = '';
+  const ashState = [];
+  latestAshData.forEach(function (data) {
+    // message+=`${data.hours?('+'+data.hours+'h'):'current'} : ${
+    let isInside = inside( currentLocation, data.pointers );
+    ashState.push(isInside);
+    message+=`${
+      isInside?'inside':'outside'
+    } @ ${updatedMoment.clone().add(data.hours,'h').format('HH a Do MMM')}\n`
+  });
+  
+  return {
+    ashState,
+    message,
+    emoMessage : ashState.map((isInside) => isInside?'😷':'😃').join(' ')
+  }
 }
